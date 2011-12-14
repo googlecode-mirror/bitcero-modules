bdcf516841004cba9078e567eaedfa78857d2bb9
mambax7@gmail.com <mambax7@gmail.com@c12da5ec-a472-11de-afd7-c7ca60a6e2ec>
2011-12-14 05:16:26 +0000

    Translations fixes

--- rmcommon/trunk/language/english/modinfo.php
+++ rmcommon/trunk/language/english/modinfo.php
@@ -19,14 +19,14 @@ define('_MI_RMC_EDITOR_VISUAL',__('Visual Editor','rmcommon'));
 define('_MI_RMC_EDITOR_HTML',__('HTML Editor','rmcommon'));
 define('_MI_RMC_EDITOR_XOOPS',__('XoopsCode Editor','rmcommon'));
 define('_MI_RMC_EDITOR_SIMPLE',__('Simple Editor','rmcommon'));
-define('_MI_RMC_IMGCATSNUMBER',__('Number limit for image categories list.','rmcommon'));
-define('_MI_RMC_IMGSNUMBER',__('Image manager number of images per page','rmcommon'));
+define('_MI_RMC_IMGCATSNUMBER',__('Limit for image categories list.','rmcommon'));
+define('_MI_RMC_IMGSNUMBER',__('Image manager: number of images per page','rmcommon'));
 define('_MI_RMC_SECREY',__('Secret Key','rmcommon'));
-define('_MI_RMC_SECREYD',__('Provide a secret key used to encript information.','rmcommon'));
+define('_MI_RMC_SECREYD',__('Provide a secret key used to encrypt information.','rmcommon'));
 define('_MI_RMC_ADMTHEME', __('Admin theme','rmcommon'));
 
 define('_MI_RMC_DOHTML',__('Allow HTMl in text','rmcommon'));
-define('_MI_RMC_DOSMILE',__('Allow smiles in text','rmcommon'));
+define('_MI_RMC_DOSMILE',__('Allow smilies in text','rmcommon'));
 define('_MI_RMC_DOXCODE',__('Allow XoopsCode','rmcommon'));
 define('_MI_RMC_DOIMAGE',__('Allow images in text','rmcommon'));
 define('_MI_RMC_DOBR',__('Auto add line breaks in text','rmcommon'));
@@ -42,7 +42,7 @@ define('_MI_RMC_ALLOWEDIT',__('Allow users to edit their comments','rmcommon'));
 define('_MI_RMC_EDITLIMIT',__('Time limit to edit a comment (in hours).','rmcommon'));
 
 // RSS
-define('_MI_RMC_RSSIMAGE', __('Image for rss feeds','rmcommon'));
+define('_MI_RMC_RSSIMAGE', __('Image for RSS feeds','rmcommon'));
 
 // Mailer Settings
 define('_MI_RMC_MAILERMETH', __('Mailer method','rmcommon'));
@@ -53,12 +53,12 @@ define('_MI_RMC_SENDMAIL', __('Sendmail','rmcommon'));
 define('_MI_RMC_SMTPSERVER',__('SMTP server to use','rmcommon'));
 define('_MI_RMC_SMTPSERVERD', __('Specify the server through with the emails will be sent.','rmcommon'));
 define('_MI_RMC_ENCRYPT', __('SMTP encryption','rmcommon'));
-define('_MI_RMC_ENCRYPTD', __('For SSL or TLS encryption to work your PHP installation must have appropriate OpenSSL transports wrappers.','rmcommon'));
+define('_MI_RMC_ENCRYPTD', __('For SSL or TLS encryption to work, your PHP installation must have appropriate OpenSSL transports wrappers.','rmcommon'));
 define('_MI_RMC_CRYPTNONE', __('None','rmcommon'));
 define('_MI_RMC_CRYPTSSL', __('SSL','rmcommon'));
 define('_MI_RMC_CRYPTTLS', __('TLS','rmcommon'));
 define('_MI_RMC_SMTPPORT', __('SMTP server port','rmcommon'));
-define('_MI_RMC_SMTPPORTD', __('Note that you must to write the apropiate port based on your encryption type selection.','rmcommon'));
+define('_MI_RMC_SMTPPORTD', __('Note that you must to write the appropriate port based on your encryption type selection.','rmcommon'));
 define('_MI_RMC_SMTPUSER', __('SMTP username','rmcommon'));
 define('_MI_RMC_SMTPPASS', __('SMTP password','rmcommon'));
 define('_MI_RMC_SENDMAILPATH', __('Sendmail path','rmcommon'));
