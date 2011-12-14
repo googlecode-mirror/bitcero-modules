4560aa9508c464a3f5f6a50f393f64db8d6002a5
mambax7@gmail.com <mambax7@gmail.com@c12da5ec-a472-11de-afd7-c7ca60a6e2ec>
2011-12-14 06:37:21 +0000

    Translations fixes

--- contact/trunk/language/english/modinfo.php
+++ contact/trunk/language/english/modinfo.php
@@ -13,15 +13,15 @@ load_mod_locale('contact');
 define('_MI_CT_DESC', __('A simple contact module for Xoops','contact'));
 
 // Settings
-define('_MI_CT_EMAIL',__('Recipient Email','contact'));
-define('_MI_CT_EMAILD',__('Specify the email where messages will be sent','contact'));
+define('_MI_CT_EMAIL',__('Email Recipient','contact'));
+define('_MI_CT_EMAILD',__('Specify the email where messages will be sent to','contact'));
 define('_MI_CT_INFO', __('Information message','contact'));
 define('_MI_CT_URL', __('Module URL', 'contact'));
 define('_MI_CT_URLD', __('Specify here the URL where ContactMe! will work. This will be used for redirections and other things.','contact'));
 define('_MI_CT_LIMIT',__('Messages per page','contact'));
 define('_MI_CT_LIMITD',__('This value specifies the limit of messages to show on each page in dashboard.','contact'));
 define('_MI_CT_QUOTE', __('Quote message text when reply','contact'));
-define('_MI_CT_QUOTED', __('When this option is enabled, ContactMe! will include the original message as a quote text in reply.','contact'));
+define('_MI_CT_QUOTED', __('When this option is enabled, ContactMe! will include the original message as a quoted text in reply.','contact'));
 
 // Block
 define('_MI_CT_BLOCK',__('ContactMe!','contact'));
